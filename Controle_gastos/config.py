c9ed0597d042b809cb266d40062980cbab50c923
apolo araujo <apolo.gab021@gmail.com>
2026-06-21 15:21:36 -0300

    config

---
+++ Controle_gastos/config.py
@@ -0,0 +1,11 @@
+import os
+
+#caminhos
+Base_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_path = os.path.join(Base_DIR, "Database", "gastos.db" )
+
+appearence = "dark"
+color_theme = "blue"
+
+Largura_tela = 900
+Altura_tela = 600
